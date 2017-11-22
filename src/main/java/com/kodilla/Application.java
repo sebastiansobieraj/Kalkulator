f994b26f7763ff452aac464a6bbca7894107addd
sebastiansobieraj <sebastian.sobieraj91@gmail.com>
2017-11-22 13:27:57 +0100

    Kalkulator

---
+++ src/main/java/com/kodilla/Application.java
@@ -0,0 +1,10 @@
+package com.kodilla;
+
+public class Application {
+    public static void main (String args []){
+        Kalkulator simpleCalcualtor = new Kalkulator();
+
+        simpleCalcualtor.addition();
+        simpleCalcualtor.subtraction();
+    }
+}
--- src/main/java/com/kodilla/Kalkulator.java
+++ src/main/java/com/kodilla/Kalkulator.java
@@ -1,4 +1,16 @@
 package com.kodilla;
 
 public class Kalkulator {
+    int a = 10;
+    int b = 15;
+
+    public void addition (){
+        double c = a + b;
+        System.out.println("Wynik dodawania to: " + c);
+    }
+
+    public void subtraction () {
+        double d = a - b;
+        System.out.println("Wynik odejmowania to: " + d);
+    }
 }
