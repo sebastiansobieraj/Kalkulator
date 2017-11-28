a051f11c606ec1fcb92615dec2746072b2b86dd5
sebastiansobieraj <sebastian.sobieraj91@gmail.com>
2017-11-28 18:26:04 +0100

    multiplication and division metods

--- src/main/java/com/kodilla/Kalkulator.java
+++ src/main/java/com/kodilla/Kalkulator.java
@@ -13,4 +13,12 @@ public class Kalkulator {
         double d = a - b;
         System.out.println("Wynik odejmowania to: " + d);
     }
+    public void multiplication () {
+        double e = a * b;
+        System.out.println("Wynik odejmowania to: " + e);
+    }
+    public void division () {
+        double f = a / b;
+        System.out.println("Wynik odejmowania to: " + f);
+    }
 }
